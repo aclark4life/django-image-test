78bed1adc1b3ba48281ff96d9f50b73c5cde8cca
Alex Clark <aclark@aclark.net>
2019-08-26 17:30:30 -0400

    Update

--- project/app/urls.py
+++ project/app/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-
 from .views import HomePageView
+from .views import CreatePostView
+
 
 urlpatterns = [
     path('', HomePageView.as_view(), name='home'),
+    path('post/', CreatePostView.as_view(), name='add_post')
 ]
